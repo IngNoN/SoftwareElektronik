a0720ed5c760bde6555dca37cd72ca352073810e
IngNoN <sebastian.karner@bulme.at>
2022-02-08 15:17:41 +0100

    implement 'control_motor' function

--- Motoransteuerung.c/Motoransteuerung.c.ino
+++ Motoransteuerung.c/Motoransteuerung.c.ino
@@ -1,30 +1,38 @@
 //Motor Pins
-
 int motorPins[4][2];
-
-int pin = 46;
-
-
-
 int speed = 1023;
+
+void control_motor(int motor_pin)
+{
+  analogWrite(motor_pin, speed);
+  while (true)
+  {
+    if (true)//(inputpin) //sensor signal von Benedikt - true durch inputpin ersetzenbei implementierung der Sensoren
+    {
+      delay(4000); //zu entfernen bei implementierung der Sensoren
+      analogWrite(motor_pin, 0);
+      break;  
+    }
+  }
+}
+
 
 void setup(){
 
-for(int motor_index = 0; motor_index < 4; motor_index++)
-{
-    for(int direction_index = 0; direction_index < 2; direction_index++)
-    {
-        pinMode(pin, OUTPUT);
-        motorPins[motor_index][direction_index] = pin++;
-        
-    }
-}
+int pin = 46;
+
+  for(int motor_index = 0; motor_index < 4; motor_index++)
+  {
+      for(int direction_index = 0; direction_index < 2; direction_index++)
+      {
+          pinMode(pin, OUTPUT);
+          motorPins[motor_index][direction_index] = pin++;
+      }
+  }
 
 Serial.begin(9600);
 
 }
-
-
 
 void loop()
 {
@@ -32,28 +40,20 @@ void loop()
   if (Serial.available() > 0) {
     input = Serial.read();
   }
-
+  
   switch(input)
   { 
     case '1':
-      analogWrite(motorPins[0][0], speed);
-      delay(3000);
-      analogWrite(motorPins[0][0], 0);
+      control_motor(motorPins[0][0]);
       break;
      case '2':
-      analogWrite(motorPins[1][0], speed);
-      delay(3000);
-      analogWrite(motorPins[1][0], 0);
+      control_motor(motorPins[1][0]);
       break;
      case '3':
-      analogWrite(motorPins[2][0], speed);
-      delay(3000);
-      analogWrite(motorPins[2][0], 0);
+      control_motor(motorPins[2][0]);
       break;
      case '4':
-      analogWrite(motorPins[3][0], speed);
-      delay(3000);
-      analogWrite(motorPins[3][0], 0);
+      control_motor(motorPins[3][0]);
       break;
   }
 }
