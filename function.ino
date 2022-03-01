0ecfe58e39ddce1f7f6302802d239fb28eb7727e
fifthaccess <benedikt.erkner-sach@bulme.at>
2022-03-01 12:42:00 +0100

    01.03.2022

--- function.ino
+++ function.ino
@@ -1,3 +1,13 @@
+
+inputPins[5] = [23,25,27,29,31];
+
+void setup8()
+{
+  for(int i = 0; i < 5 ; i++)
+    {
+      pinMode(inputPins[i], INPUT);
+    }
+}
 void waitForSensor (int inputPin)
 {
     while(true)
